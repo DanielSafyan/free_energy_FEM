19b29084b88e24757fd6a1e82d395971ad8b0bd5
DanielSafyan <danielsafyan@gmail.com>
2025-09-03 15:31:30 +0100

    nochmal fix platform calc

--- pong_simulation/pong_sim_npen.py
+++ pong_simulation/pong_sim_npen.py
@@ -684,10 +684,10 @@ def calculate_platform_position2(measured_current, screen_height):
     I2 = measured_current[1]/denominator
     I3 = measured_current[2]/denominator
     # Map to 3 equally spaced vertical anchors: 0, H/3, 2H/3
-    h_third = float(screen_height) / 3.0
+    h_third = np.floor(float(screen_height) / 3.0)
     # Max of 2nd degree polynomial through points
     a,b,c = np.polyfit([0, h_third, 2*h_third], [I1, I2, I3], 2)
-    x = np.linspace(0, screen_height, screen_height)
+    x = np.linspace(0, 2*h_third, 2*h_third)
     y = a*x**2 + b*x + c
     return np.argmax(y)
 
